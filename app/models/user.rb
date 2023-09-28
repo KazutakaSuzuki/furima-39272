d31b61c612fcdd96bfa01d1a824209b9d29b0c55
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-09-28 22:48:14 +0900

    エラーメッセージの追加　mail&pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
 
 
-  validates :nickname,   presence: true
+  validates :nickname,  presence: true
   validates :kanji_sei, presence: true
   validates :kanji_mei, presence: true
   validates :kana_sei,  presence: true
   validates :kana_mei,  presence: true
   validates :birthday,  presence: true
+  validates :password,  presence: true, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
 
 end
