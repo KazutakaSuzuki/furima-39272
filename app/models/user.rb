470772b5a3c3ea9a832b67d40bb8f7a6faca5733
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-09-30 22:30:18 +0900

    userモデルのテストコード記述2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,
+         :validatable
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
          VALID_FULLWIDTH_REGEX = /\A[ぁ-んァ-ン一-龯々]+\z/
          VALID_KANAWIDTH_REGEX = /\A[ァ-ン]+\z/
-
 
   validates :nickname,         presence: true
   validates :birthday,         presence: true
@@ -14,6 +14,6 @@ class User < ApplicationRecord
   validates :first_name,       presence: true, format: { with: VALID_FULLWIDTH_REGEX, message: "is invalid. Input full-width characters" }
   validates :last_name_kana,   presence: true, format: { with: VALID_KANAWIDTH_REGEX, message: "is invalid. Input full-width katakana characters" }
   validates :first_name_kana,  presence: true, format: { with: VALID_KANAWIDTH_REGEX, message: "is invalid. Input full-width katakana characters" }
-  validates :password,         presence: true, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
+  validates :password,         presence: true, format: { with: VALID_PASSWORD_REGEX,  message: "is invalid. Include both letters and numbers" }
 
 end
