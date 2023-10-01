eae04ec89e1818e76f6f91d777e0aae5fa11b7bb
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-01 21:52:05 +0900

    userモデルのテストコード記述3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,26 @@
 FactoryBot.define do
-  factory :user do
+
+ factory :user do
+    
     nickname                  {Faker::Name.last_name}
     email                     {Faker::Internet.free_email}
     password                  {Faker::Internet.password(min_length: 6)}
     password_confirmation     {password}
+    birthday                  {Faker::Date.birthday}
+    last_name                 {Faker::Name.last_name}
+    first_name                {Faker::Name.first_name}
+
+    to_create do |instance|
+      class << instance
+        def katakana_name
+          %w[アイウエオ カキクケコ サシスセソ タチツテト ナニヌネノ ハヒフヘホ マミムメモ ラリルレロ].sample
+        end
+      end
+      instance.save!
+    end
+
+
+    last_name_kana            {Faker::Name.last_name_kana(katakana_name)}
+    first_name_kana           {Faker::Name.first_name_kana(katakana_name)} 
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'メールアドレスが一意性であること' do
+      @user = FactoryBot.build(:user)
       @user.save 
-
       another_user = FactoryBot.build(:user, email: @user.email) 
       another_user.valid?
       expect(another_user.errors.full_messages).to include("Email has already been taken")
