f8b11f2f26d69a3fd11480e183ba1d13a0189246
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-04 17:34:23 +0900

    ActiveHash導入/バリデーション

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :fee_status
+  belongs_to :prefecture
+  belongs_to :schedule
+
+  validates :title, :text, presence: true
+  validates :category_id, :quality_id, :fee_status_id, :prefecture_id, :schedule_id, numericality: {other_than: 1, message: "can't be blank"}
+end
---
+++ db/migrate/20231004065020_create_articles.rb
@@ -0,0 +1,15 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+
+      t.string     :title,         null: false
+      t.text       :text,          null: false
+      t.integer    :category_id,   null: false
+      t.integer    :quality_id,    null: false
+      t.integer    :fee_status_id, null: false
+      t.integer    :prefecture_id, null: false
+      t.integer    :schedule_id,   null: false
+      t.timestamps
+    end
+  end
+end
