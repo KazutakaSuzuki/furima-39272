66ba5bd5c40ab66c20d55827bf11356f31ce4aaf
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-04 15:26:53 +0900

    モデルとテーブル作成

---
+++ db/migrate/20231004061012_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+
+      t.timestamps
+      t.string :item_name,     null: false
+      t.text    :explain,       null: false
+      t.integer :category_id,   null: false
+      t.integer :quality_id,    null: false
+      t.integer :fee_status_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :scheduled_id,  null: false
+      t.integer :price,         null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_26_102722) do
+ActiveRecord::Schema.define(version: 2023_10_04_061012) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "item_name", null: false
+    t.text "explain", null: false
+    t.integer "category_id", null: false
+    t.integer "quality_id", null: false
+    t.integer "fee_status_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "scheduled_id", null: false
+    t.integer "price", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
