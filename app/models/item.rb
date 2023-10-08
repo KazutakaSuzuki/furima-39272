fa7a24c208f20a38c838a27d60e8cbced0baf5f5
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-08 17:45:34 +0900

    モデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,12 +13,13 @@ class Item < ApplicationRecord
 
   validates :image, presence: true
   validates :item_name,      presence: true
-  validates :quality_id,     numericality: { other_than: 1, message: "can't be blank"}
+  validates :explain,        presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  validates :price,                 numericality: {only_integer: true, message: "is invalid. Input half-width characters"}
   validates :category_id,    numericality: { other_than: 1, message: "can't be blank"}
+  validates :quality_id,     numericality: { other_than: 1, message: "can't be blank"}
   validates :fee_status_id,  numericality: { other_than: 1, message: "can't be blank"}
   validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank"}
   validates :schedule_id,    numericality: { other_than: 1, message: "can't be blank"}
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-  validates :price,                 numericality: {only_integer: true, message: "is invalid. Input half-width characters"}
 
 end
