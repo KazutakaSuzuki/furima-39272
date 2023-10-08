bae2a71c6add57123544ac6861a1818e6a5aec37
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-08 23:53:22 +0900

    販売手数料/販売利益の追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+
+    association :user
+    item_name { Faker::Commerce.product_name }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+ 
+    explain             { 'あああああああああ' }
+    category_id         { 2 }
+    quality_id          { 2 }
+    fee_status_id       { 2 }
+    prefecture_id       { 2 }
+    schedule_id         { 2 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
