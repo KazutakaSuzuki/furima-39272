1a6f209bca624c602c64e06ea1ff7c9e06631556
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-09 18:46:50 +0900

    レビュー後テスト追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :fee_status
   belongs_to :prefecture
   belongs_to :schedule
-  has_one :order
+  #has_one :order
   has_one_attached :image
 
   validates :image, presence: true
