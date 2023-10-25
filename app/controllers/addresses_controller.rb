bd19bd0aa492a58180ceb0fe3e3539d0dc5213a6
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-26 03:32:11 +0900

    購入内容画面の表示

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,3 @@
 class AddressesController < ApplicationController
 
-  def index
-  end
-
-  def create
-    @address = OrderAddress.new(set_paramms)
-  end
-
-  private
-
-  def set_params
-    params.permit()
-  end
-
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,21 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new
   end
 
-  def show
-    Order.show(order.params)
+  def create
+    @item = Item.find(params[:item_id])
+    @order = @item.orders.create(order_params)
+    @order = OrderAddress.new(order_params)
+
   end
 
   private
   def order_params
-    params.require(:order).permit.(:text).merge(params[:item_id])
+    params.require(:order_address).permit(:postarl_code, :prefecture_id, :city, :add_number, 
+      :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
 
   end
 end
