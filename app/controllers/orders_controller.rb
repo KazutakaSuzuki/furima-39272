b09207d4a78d9e791e94bccd8ebd5faeb731e7ff
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-10-26 15:36:02 +0900

    コントローラー/ビュー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,16 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = OrderAddress.new
+    @address = Address.new
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @order = OrderAddress.new(order_params)
-    if @order.valid?
+    @address = Address.new(order_params)
+    binding.pry
+    if @address.valid?
       pay_item
-      @order.save
+      @address.save
       return redirect_to root_path      
     else
       render 'index'
@@ -22,14 +23,14 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_address).permit(:postarl_code, :prefecture_id, :city, :add_number, 
-      :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code,
+      :prefecture_id, :city, :add_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
         Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  #PAY.JP秘密鍵
         Payjp::Charge.create(
-          amount: order_params[:price],  #商品の値段
+          amount: @item.price,  #商品の値段
           card: order_params[:token],    #カードトークン
           currency: 'jpy'                #通貨の種類(日本円)
         )
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,10 @@
 class OrderAddress < ApplicationRecord
   include ActiveModel::Model
   belongs_to :prefecture
-  attr_accessor :token, :postarl_code, :prefecture_id, :city, :add_number, 
-                :building, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :add_number, 
+                :building, :phone_number, :item_id, :user_id, :token
   
   with_options presence: true do
-  
-    validates :token
 
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -15,12 +13,14 @@ class OrderAddress < ApplicationRecord
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid." }
     validates :user_id
     validates :item_id
+  
+    validates :token
   end
 
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, add_number: add_number, 
-                   building: buildeing, phone_number: phone_number, order_id: order.id)
+                   building: building, phone_number: phone_number, order_id: order.id)
   end
 end
