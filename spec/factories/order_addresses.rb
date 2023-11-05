113473d172cc96bfb659bb32351ce2933e5aa942
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-11-05 23:53:24 +0900

    テストコード作成1

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order_address do
-    
+
+    postal_code           { '999-0000' }
+    prefecture_id         { 6 }
+    city                  { '長井市' }
+    add_number            { '22-33-44' }
+    phone_number          { '09000009999' }
+
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order_address = FactoryBot.build(:order_address)
+  end
+
+  describe '商品購入機能' do
+    context '商品が購入できるとき' do
+      it 'クレジットカード情報と配送先があれば購入できる' do
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '商品を購入できないとき' do
+      it '郵便番号がないとき' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '都道府県が入ってないとき' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市区町村が入ってないとき' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が入ってないとき' do
+        @order_address.add_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Add number can't be blank")
+      end
+      it '電話番号が入ってないとき' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+    end
+  end
+
+    
 end
