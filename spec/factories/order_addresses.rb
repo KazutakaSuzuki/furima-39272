85a49f13194b7f81a6da85bb70f0916ffe945c51
KazutakaSuzuki <kazutakasuzuki0708@gmail.com>
2023-11-06 21:46:54 +0900

    テスト完了 1回目

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,11 +1,17 @@
 FactoryBot.define do
   factory :order_address do
 
+
+    user_id               { 10 }
+    item_id               { 10 }
+
     postal_code           { '999-0000' }
     prefecture_id         { 6 }
     city                  { '長井市' }
     add_number            { '22-33-44' }
+    building              { '〇〇アパート' }
     phone_number          { '09000009999' }
+    token { '3530111333300000' }
 
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -7,16 +7,26 @@ RSpec.describe OrderAddress, type: :model do
 
   describe '商品購入機能' do
     context '商品が購入できるとき' do
-      it 'クレジットカード情報と配送先があれば購入できる' do
+      it 'トークンと配送先情報があれば購入できる' do
         expect(@order_address).to be_valid
       end
     end
 
     context '商品を購入できないとき' do
-      it '郵便番号がないとき' do
+      it '郵便番号が入ってないとき' do
         @order_address.postal_code = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号にハイフンがないとき' do
+        @order_address.postal_code = '9990000'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+      it '郵便番号の数字が少ないとき' do
+        @order_address.postal_code = '999'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
       end
       it '都道府県が入ってないとき' do
         @order_address.prefecture_id = ''
@@ -41,6 +51,4 @@ RSpec.describe OrderAddress, type: :model do
 
     end
   end
-
-    
 end
